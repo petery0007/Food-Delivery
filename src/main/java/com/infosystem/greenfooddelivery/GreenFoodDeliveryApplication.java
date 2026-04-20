91eca00617afd2be360d1357326cbcd5705870e6
petery <2126418018@qq.com>
2026-04-20 18:57:25 +0800

    complete the front-end architecture

--- src/main/java/com/infosystem/greenfooddelivery/GreenFoodDeliveryApplication.java
+++ src/main/java/com/infosystem/greenfooddelivery/GreenFoodDeliveryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GreenFoodDeliveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GreenFoodDeliveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GreenFoodDeliveryApplication.class, args);
+    }
 
 }
