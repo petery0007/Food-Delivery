0b57a52589b876f744ff7d07a66a4fd16652f719
petery <2126418018@qq.com>
2026-04-20 16:16:43 +0800

    Initial commit

---
+++ src/main/java/com/infosystem/greenfooddelivery/GreenFoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.infosystem.greenfooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenFoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenFoodDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infosystem/greenfooddelivery/GreenFoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosystem.greenfooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenFoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
